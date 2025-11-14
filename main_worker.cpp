f31991ad176de7be1fba654812f509a2bc175194
PPoonYaPat <poonyapat4404@gmail.com>
2025-11-15 04:04:52 +0800

    Edit worker

--- main_worker.cpp
+++ main_worker.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    assert(argc==2);
+    assert(argc==2 || argc==3);
     int listening_port=atoi(argv[1]);
 
+    bool download_testcases = false;
+    if (argc==3 && string(argv[2])=="-tc") download_testcases = true;
+
     Worker worker(listening_port);
-    worker.init_task(false);
+    worker.init_task(download_testcases);
     worker.start();
 }
